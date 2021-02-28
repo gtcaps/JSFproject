131a211461a4f6a88b6d81d06a1b7ea53518ec73
Aybson Mercado <aybsonmg@gmail.com>
2021-02-28 01:33:38 -0600

    JSFproject Version 1.1 Se crea el login del aplicativo y se redirecciona a la pagina principal

--- src/main/java/com/admercad/p/controllers/LoginController.java
+++ src/main/java/com/admercad/p/controllers/LoginController.java
@@ -1,12 +1,18 @@
 package com.admercad.p.controllers;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 public class LoginController {
-    
+
     private String usuario;
-    
+
     private String password;
 
     public String getUsuario() {
@@ -24,9 +30,25 @@ public class LoginController {
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
     public void ingresar() {
-        System.out.println("Usuario: " +  usuario);
+        if (usuario.equals("admin") && password.equals("admin")) {
+            System.out.println("Usuario: " + usuario);
+            try {
+                this.redireccionar("principal.xhtml");
+            } catch (IOException ex) {
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+                FacesContext.getCurrentInstance().addMessage("formLogin:txtUsuario", 
+                        new FacesMessage(FacesMessage.SEVERITY_FATAL, "La pagina no se encuentra", ""));
+            }
+        } else {
+            FacesContext.getCurrentInstance().addMessage("formLogin:txtPassword", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuario Incorrecto", ""));
+        }
     }
-    
+
+    private void redireccionar(String pagina) throws IOException {
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+        ec.redirect(pagina);
+    }
+
 }
